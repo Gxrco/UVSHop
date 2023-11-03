647cf48a64c2a71a03b8dafc9618ee1d660da066
Gerson Ramírez (Gerco) <93165594+Gxrco@users.noreply.github.com>
2023-11-02 20:07:14 -0600

    SelectedProductView created with composable implementation

---
+++ app/src/main/java/com/example/uvshop/UIView/SelectedProductScreen/View/SelectedProductView.kt
@@ -0,0 +1,141 @@
+package com.example.uvshop.UIView.SelectedProductScreen.View
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.uvshop.R
+
+class SelectedProductView {
+    @SuppressLint("NotConstructor")
+    @Preview
+    @Composable
+    fun SelectedProductView(){
+        Box(Modifier.fillMaxSize()){
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+            ){
+                item{
+                    Image(
+                        painter = painterResource(id = R.drawable.product),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Text(
+                        text = stringResource(id = R.string.titulo),
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.padding(horizontal = 15.dp)
+                    )
+                    Spacer(Modifier.height(15.dp))
+                    Text(
+                        text = stringResource(id = R.string.tienda),
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Medium,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.padding(horizontal = 15.dp)
+                    )
+                    Spacer(Modifier.height(25.dp))
+                    Text(
+                        text = stringResource(id = R.string.loremextend),
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Normal,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier.padding(horizontal = 15.dp),
+
+                    )
+                }
+            }
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.TopStart)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back",
+                    tint = Color.White
+                )
+            }
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.TopEnd)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Favorito",
+                    tint = Color.White
+                )
+            }
+            Box(modifier = Modifier.shadow(elevation = 10.dp, spotColor = Color.Black).align(Alignment.BottomCenter).fillMaxWidth()) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFFF1F1F1))
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.precioreal),
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                    )
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(10),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0BA259))
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.anadiralcarrito),
+                            color = Color.White,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/uvshop/UIView/SelectedProductScreen/ViewModel/SelectedProductViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.uvshop.UIView.SelectedProductScreen.ViewModel
+
+class SelectedProductViewModel {
+}
